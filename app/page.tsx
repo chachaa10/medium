c5f7420f83c127e84fdeeca4adc191fa0b0c8198
Chalex <177287665+chalexmarmol@users.noreply.github.com>
2025-10-12 19:46:45 +0800

    feat: add posts actions

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
+import { getPosts } from "@/app/actions/post.actions";
 import Header from "@/app/components/layout/header";
 import { ThemeToggle } from "@/app/components/theme-button";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await getPosts();
   return (
     <>
       <Header>
         {/* <ThemeToggle /> */}
         {/*  */}
       </Header>
-      <main></main>
+      <main>
+        <h1>Home Page</h1>
+
+        {posts.map((post) => {
+          return (
+            <div key={post.postId}>
+              <h2>{post.title}</h2>
+              <p>{post.content}</p>
+            </div>
+          );
+        })}
+      </main>
     </>
   );
 }
