49ede638240f62cfc882817d7f0e6d601ba314ba
Chalex <177287665+chalexmarmol@users.noreply.github.com>
2025-10-15 10:22:17 +0800

    fix: delete unnecessary components

--- app/(user-profile)/[slug]/layout.tsx
+++
@@ -1,6 +0,0 @@
-interface UserPageLayoutProps {
-  children: React.ReactNode;
-}
-export default function UserPageLayout({ children }: UserPageLayoutProps) {
-  return <div>{children}</div>;
-}
--- app/(user-profile)/[slug]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ProfilePage({ params }: { params: { slug: string } }) {
-  return (
-    <div>
-      <h1>Profile: {params.slug}</h1>
-    </div>
-  );
-}
