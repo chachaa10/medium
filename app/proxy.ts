226296cbc522d44b6a1b7b9a38ee42c372795d30
Chalex <177287665+chalexmarmol@users.noreply.github.com>
2025-10-30 11:37:49 +0800

    feat: add proxy

--- app/proxy.ts
+++ app/proxy.ts
@@ -1,36 +1,37 @@
-// import { type NextRequest, NextResponse } from "next/server";
-// import { getCurrentUser } from "./data/user";
+import { getCurrentUser } from "@/app/data/users";
+import { type NextRequest, NextResponse } from "next/server";
 
-// // 1. Specify protected and public routes
-// const protectedRoutes = ["/", "/new-post"];
-// const publicRoutes = ["/auth/login"];
+// Specify protected and public routes
+const protectedRoutes = ["/", "/new-post", "/[author]/[posts]"];
+const publicRoutes = ["/login"];
 
-// export default async function proxy(req: NextRequest) {
-//   // 2. Check if the current route is protected or public
-//   const path = req.nextUrl.pathname;
-//   const isProtectedRoute = protectedRoutes.includes(path);
-//   const isPublicRoute = publicRoutes.includes(path);
+// TODO: fix this function
+export default async function proxy(req: NextRequest) {
+  // Check if the current route is protected or public
+  const path = req.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.includes(path);
+  const isPublicRoute = publicRoutes.includes(path);
 
-//   const session = await getCurrentUser();
+  const session = await getCurrentUser();
 
-//   // 4. Redirect to /login if the user is not authenticated
-//   if (isProtectedRoute && !session?.session.userId) {
-//     return NextResponse.redirect(new URL("/auth/login", req.nextUrl));
-//   }
+  // Redirect to /login if the user is not authenticated
+  if (isProtectedRoute && !session?.session.userId) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }
 
-//   // 5. Redirect to /dashboard if the user is authenticated
-//   if (
-//     isPublicRoute &&
-//     session?.session.userId &&
-//     !req.nextUrl.pathname.startsWith("/dashboard")
-//   ) {
-//     return NextResponse.redirect(new URL("/", req.nextUrl));
-//   }
+  // Redirect to / if the user is authenticated
+  if (
+    isPublicRoute &&
+    session?.session.userId &&
+    !req.nextUrl.pathname.startsWith("/dashboard")
+  ) {
+    return NextResponse.redirect(new URL("/", req.nextUrl));
+  }
 
-//   return NextResponse.next();
-// }
+  return NextResponse.next();
+}
 
-// // Routes Proxy should not run on
-// export const config = {
-//   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
-// };
+// Routes Proxy should not run on
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
