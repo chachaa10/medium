21294d09e958e6b8a1a6e0d3358adcfe86f5687a
Chalex <177287665+chalexmarmol@users.noreply.github.com>
2025-10-30 18:29:10 +0800

    feat: get the html of rich text editor

--- app/components/features/rich-text-editor/index.tsx
+++ app/components/features/rich-text-editor/index.tsx
@@ -1,20 +1,30 @@
 "use client";
-import { memo } from "react";
-import MenuBar from "@/app/components/features/tiptap/menu-bar";
+import MenuBar from "@/app/components/features/rich-text-editor/menu-bar";
 import Typography from "@tiptap/extension-typography";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import TitleTextArea from "@/app/components/features/tiptap/title-textarea";
+import TitleTextArea from "@/app/components/features/rich-text-editor/title-textarea";
 
-export const Tiptap = memo(() => {
+interface RichTextEditorProps {
+  content: string;
+  onChange: (content: string) => void;
+}
+export default function RichTextEditor({
+  content,
+  onChange,
+}: RichTextEditorProps) {
   const editor = useEditor({
     extensions: [StarterKit, Typography],
     immediatelyRender: false,
+    content: content,
     editorProps: {
       attributes: {
         class:
           "prose dark:prose-invert prose-sm sm:prose-base min-h-[400px] space-y-4 border border-gray-300 p-4 rounded-md min-w-full focus:outline-none prose-h2:text-xl prose-h3:text-lg prose-h4:text-md prose-p:text-base prose-p:text-neutral-900 dark:prose-p:text-neutral-200",
       },
+    },
+    onBlur: ({ editor }) => {
+      onChange(editor.getHTML());
     },
   });
 
@@ -29,6 +39,4 @@ export const Tiptap = memo(() => {
       )}
     </div>
   );
-});
-
-Tiptap.displayName = "Tiptap";
+}
--- app/new-post/page.tsx
+++ app/new-post/page.tsx
@@ -1,18 +1,26 @@
-import { Tiptap } from "@/app/components/features/tiptap";
-import SubmitPostButton from "@/app/components/features/tiptap/submit-post-button";
+"use client";
+import RichTextEditor from "@/app/components/features/rich-text-editor";
+import SubmitPostButton from "@/app/components/features/rich-text-editor/submit-post-button";
 import { redirect } from "next/navigation";
-import { getCurrentUser } from "../data/users";
+import { authClient } from "../lib/auth-client";
+import { useState } from "react";
 
-export default async function NewPostPage() {
-  const session = await getCurrentUser();
+export default function NewPostPage() {
+  const [post, setPost] = useState("");
+  const session = authClient.getSession();
 
   if (!session) {
     redirect("/login");
   }
 
+  function handleOnChange(content: string) {
+    setPost(content);
+    console.log(content);
+  }
+
   return (
     <main className="space-y-4 mx-auto p-4 w-full max-w-[80ch]">
-      <Tiptap />
+      <RichTextEditor content={post} onChange={handleOnChange} />
 
       <SubmitPostButton className="py-4 rounded-md w-full font-semibold uppercase cursor-pointer" />
     </main>
