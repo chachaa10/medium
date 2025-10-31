f64732b3b23e41edc30aece3db357722e1a9274d
Chalex <177287665+chalexmarmol@users.noreply.github.com>
2025-10-31 17:18:36 +0800

    feat: move the create new post button to header

--- app/components/layout/header.jsx
+++ app/components/layout/header.jsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import Logo from "@/app/components/features/navbar/logo";
 import Logout from "@/app/components/features/navbar/logout-button";
 import Search from "@/app/components/features/navbar/search";
 import SidebarToggle from "@/app/components/features/navbar/sidebar-toggle";
 import { ThemeToggle } from "@/app/components/features/navbar/theme-button";
 import { getCurrentUser } from "@/app/data/users";
+import WriteIcon from "../features/navbar/write-icon";
+import { Button } from "../ui/button";
 
 export default async function Header() {
   const session = await getCurrentUser();
@@ -19,8 +22,17 @@ export default async function Header() {
       ) : (
         <Logo className="ml-4" />
       )}
-      <div className="flex gap-2 pr-4">
-        {session && <Logout />}
+      <div className="flex gap-4 pr-4">
+        {session && (
+          <>
+            <Button asChild variant={"outline"}>
+              <Link href="/new-post">
+                <WriteIcon />
+              </Link>
+            </Button>
+            <Logout />
+          </>
+        )}
         <ThemeToggle />
       </div>
     </header>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Link from "next/link";
 import { redirect } from "next/navigation";
 import { Suspense } from "react";
 import PostList from "@/app/components/features/post/post-list";
 import PostSkeleton from "@/app/components/features/post/skeleton";
-import { Button } from "@/app/components/ui/button";
 import { getCurrentUser } from "@/app/data/users";
 
 export default async function HomePage() {
@@ -16,12 +14,6 @@ export default async function HomePage() {
   return (
     <main className="mx-auto p-2 w-full max-w-[80ch] container">
       <h1 className="font-bold text-4xl">Home Page</h1>
-      <Button
-        asChild
-        className="bg-green-700 hover:bg-green-800 text-neutral-50"
-      >
-        <Link href="/new-post">New Post</Link>
-      </Button>
 
       <Suspense fallback={<PostSkeleton />}>
         <PostList />
